199fafe41503ed28cdaaba5068fda48c1b117bdd
unknown <elard.sd.9@gmail.com>
2020-05-24 19:16:45 -0500

    add activity map 2

--- Laboratorio08/app/src/main/java/pe/com/tecsup/laboratorio08/MapsActivity2.java
+++ Laboratorio08/app/src/main/java/pe/com/tecsup/laboratorio08/MapsActivity2.java
@@ -3,13 +3,22 @@ package pe.com.tecsup.laboratorio08;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import pe.com.tecsup.laboratorio08.models.ResponsePlace;
+
+import pe.com.tecsup.laboratorio08.service.API;
+import pe.com.tecsup.laboratorio08.service.ServiceGenerator;
+import retrofit2.Call;
+import android.util.Log;
+
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,9 +48,26 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        API client = ServiceGenerator.createService(API.class);
+        Call<ResponsePlace> call = client.getDataMarkers("-12.046373, -77.042755","50000","restaurant","pollo%20a%20la%20brasa","AIzaSyAn8DpxSG8yU35XhtDeS5R_eMvBI8XXm2g");
+        call.enqueue(new Callback<ResponsePlace>() {
+            @Override
+            public void onResponse(Call<ResponsePlace> call, Response<ResponsePlace> response) {
+                for (Result result : response.body().getResults()){
+                    LatLng markerLocation = new LatLng(
+                            result.getGeometry().getLocation().getLat(),
+                            result.getGeometry().getLocation().getLng());
+                    String markerTitle = result.getName();
+
+                    mMap.addMarker(new MarkerOptions().position(markerLocation).title(markerTitle));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponsePlace> call, Throwable t) {
+                Toast.makeText(MapsActivity2.this,"No se puedo recuperar la localización",Toast.LENGTH_SHORT).show();
+                Log.e(this.getClass().getName(), t.getMessage());
+            }
+        });
     }
 }
--- Laboratorio08/app/src/main/java/pe/com/tecsup/laboratorio08/models/ResponsePlace.java
+++ Laboratorio08/app/src/main/java/pe/com/tecsup/laboratorio08/models/ResponsePlace.java
@@ -1,4 +1,76 @@
 package pe.com.tecsup.laboratorio08.models;
 
-public class ResponsePlace {
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public abstract class ResponsePlace {
+
+    private Results results;
+
+    public Results getResults() {
+        return results;
+    }
+
+    public void setResults(Results results) {
+        this.results = results;
+    }
+
+    public static class Results {
+        private String name;
+        private Geometry geometry;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Geometry getGeometry() {
+            return geometry;
+        }
+
+        public void setGeometry(Geometry geometry) {
+            this.geometry = geometry;
+        }
+    }
+
+    public static class Geometry {
+        private Location location;
+
+        public Location getLocation() {
+            return location;
+        }
+
+        public void setLocation(Location location) {
+            this.location = location;
+        }
+    }
+
+    public static class Location {
+        private double lng;
+        private double lat;
+
+        public double getLng() {
+            return lng;
+        }
+
+        public void setLng(double lng) {
+            this.lng = lng;
+        }
+
+        public double getLat() {
+            return lat;
+        }
+
+        public void setLat(double lat) {
+            this.lat = lat;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return new ToStringBuilder(this).append("results",results).toString();
+    }
 }
